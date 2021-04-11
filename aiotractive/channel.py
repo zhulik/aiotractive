9e7155f722dcacc8b26a279a199ed780a712ba8e
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2021-04-12 00:15:29 +0200

    Ignore handshake events, properly handle timeouts

--- aiotractive/channel.py
+++ aiotractive/channel.py
@@ -1,18 +1,25 @@
 import json
 
+from asyncio.exceptions import TimeoutError
+
 
 class Channel:
     CHANNEL_URL = "https://channel.tractive.com/3/channel"
+    IGNORE_MESSAGES = ["handshake", "keep-alive"]
 
     def __init__(self, api):
         self._api = api
 
     async def listen(self):
-        async with self._api.session.request(
-            "POST", self.CHANNEL_URL, headers=await self._api.auth_headers()
-        ) as response:
-            async for data, _ in response.content.iter_chunks():
-                event = json.loads(data)
-                if event["message"] == "keep-alive":
-                    continue
-                yield event
+        while True:
+            try:
+                async with self._api.session.request(
+                    "POST", self.CHANNEL_URL, headers=await self._api.auth_headers()
+                ) as response:
+                    async for data, _ in response.content.iter_chunks():
+                        event = json.loads(data)
+                        if event["message"] in self.IGNORE_MESSAGES:
+                            continue
+                        yield event
+            except TimeoutError:
+                continue
