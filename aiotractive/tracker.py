20fe0be6bc63b44e3fedbc25c0e2533ca39132bf
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2021-04-12 00:39:47 +0200

    Add control actions for led, buzzer and live tracking

--- aiotractive/tracker.py
+++ aiotractive/tracker.py
@@ -6,6 +6,8 @@ from .data_object import DataObject
 class Tracker(DataObject):
     DEFAULT_PERIOD_HOURS = 6
 
+    ACTIONS = {True: "on", False: "off"}
+
     async def hw_info(self):
         return await self._api.request(f"device_hw_report/{self.id}/")
 
@@ -19,4 +21,25 @@ class Tracker(DataObject):
                 "time_to": round(time_to.timestamp()),
                 "format": "json_segments",
             },
+        )
+
+    async def set_buzzer_active(self, active):
+        action = self.ACTIONS[active]
+
+        return await self._api.request(
+            f"tracker/{self.id}/command/buzzer_control/{action}"
+        )
+
+    async def set_led_active(self, active):
+        action = self.ACTIONS[active]
+
+        return await self._api.request(
+            f"tracker/{self.id}/command/led_control/{action}"
+        )
+
+    async def set_live_tracking_active(self, active):
+        action = self.ACTIONS[active]
+
+        return await self._api.request(
+            f"tracker/{self.id}/command/live_tracking/{action}"
         )
