edce652d6dfe3821038ddb57b65eafeb05acd6dc
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2021-05-19 00:21:41 +0200

    Add Tracker.pos_report

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="aiotractive",
-    version="0.1.1",
+    version="0.2.0",
     author="Gleb Sinyavskiy",
     author_email="zhulik.gleb@gmail.com",
     description="Asynchronous Python client for the Tractive REST API",
